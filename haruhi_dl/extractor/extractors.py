5a6c27fd90c80eeee07ce1f02712163b13dd998e
Remita Amine <remitamine@gmail.com>
2021-02-26 16:08:20 +0100

    [applepodcasts] Add new extractor(#25918)

--- haruhi_dl/extractor/extractors.py
+++ haruhi_dl/extractor/extractors.py
@@ -65,6 +65,7 @@ from .appletrailers import (
     AppleTrailersIE,
     AppleTrailersSectionIE,
 )
+from .applepodcasts import ApplePodcastsIE
 from .archiveorg import ArchiveOrgIE
 from .arcpublishing import ArcPublishingIE
 from .arkena import ArkenaIE
