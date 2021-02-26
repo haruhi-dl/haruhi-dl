1250d58b96733a4439a3a2a5c52505e68c1363e4
Laura Liberda <laura@selfisekai.rocks>
2021-02-26 14:27:42 +0100

    improve copykitku patch hook

--- devscripts/copykitku-patch-hook.js
+++ devscripts/copykitku-patch-hook.js
@@ -4,16 +4,18 @@
 
 module.exports = function patchHook(patchContent) {
     [
+        [/(?:youtube-|yt-?)dl\.org/g, 'haruhi.download'],
         [/youtube_dl/g, 'haruhi_dl'],
         [/youtube-dl/g, 'haruhi-dl'],
         [/youtubedl/g, 'haruhidl'],
         [/YoutubeDL/g, 'HaruhiDL'],
         [/ytdl/g, 'hdl'],
-        [/(?:youtube-|yt-?)dl\.org/g, 'haruhi.download'],
         [/yt-dl/g, 'h-dl'],
+        [/ydl/g, 'hdl'],
 
         // prevent from linking to non-existent repository
         [/github\.com\/ytdl-org\/haruhi-dl/g, 'github.com/ytdl-org/youtube-dl'],
+        [/github\.com\/rg3\/haruhi-dl/g, 'github.com/ytdl-org/youtube-dl'],
         // prevent changing the smuggle URLs (for compatibility with ytdl)
         [/__haruhidl_smuggle/g, '__youtubedl_smuggle'],
     ].forEach(([regex, replacement]) => patchContent = patchContent.replace(regex, replacement));
