e55149a2fc6ab0abbf6d6d4387172420a07c8e80
Isaac-the-Man <steven97102@gmail.com>
2021-02-26 18:17:50 +0100

    [samplefocus] Add new extractor(closes #27763)

--- haruhi_dl/extractor/extractors.py
+++ haruhi_dl/extractor/extractors.py
@@ -1074,6 +1074,7 @@ from .safari import (
     SafariApiIE,
     SafariCourseIE,
 )
+from .samplefocus import SampleFocusIE
 from .sapo import SapoIE
 from .savefrom import SaveFromIE
 from .sbs import SBSIE
