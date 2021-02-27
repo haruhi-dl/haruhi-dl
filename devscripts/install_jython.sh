0f68e2ad096546b89b5b21a1f09416441f2b08f9
Laura Liberda <laura@selfisekai.rocks>
2021-02-27 02:32:47 +0100

    remove some unused devscripts/docs

--- devscripts/install_jython.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget https://repo1.maven.org/maven2/org/python/jython-installer/2.7.2/jython-installer-2.7.2.jar
-java -jar jython-installer-2.7.2.jar -s -d "$HOME/jython"
-$HOME/jython/bin/jython -m pip install nose
