ec794dacb29c16b9c4a6dfa66db027b5bc408ca0
Lauren Liberda <laura@selfisekai.rocks>
2021-03-28 22:01:33 +0200

    --ie-key cli option

--- haruhi_dl/__init__.py
+++ haruhi_dl/__init__.py
@@ -348,6 +348,7 @@ def _real_main(argv=None):
         'restrictfilenames': opts.restrictfilenames,
         'ignoreerrors': opts.ignoreerrors,
         'force_generic_extractor': opts.force_generic_extractor,
+        'ie_key': opts.ie_key,
         'ratelimit': opts.ratelimit,
         'nooverwrites': opts.nooverwrites,
         'retries': opts.retries,
