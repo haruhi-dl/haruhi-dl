0f178530c5e3e03b8e230bd2c9920766effa24fa
Remita Amine <remitamine@gmail.com>
2021-04-18 16:37:17 +0200

    [cbssports] fix extraction(closes #28682)

--- haruhi_dl/extractor/extractors.py
+++ haruhi_dl/extractor/extractors.py
@@ -203,7 +203,11 @@ from .cbsnews import (
     CBSNewsIE,
     CBSNewsLiveVideoIE,
 )
-from .cbssports import CBSSportsIE
+from .cbssports import (
+    CBSSportsEmbedIE,
+    CBSSportsIE,
+    TwentyFourSevenSportsIE,
+)
 from .ccc import (
     CCCIE,
     CCCPlaylistIE,
