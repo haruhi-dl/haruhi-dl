ee572c5d1945e8f2161cf283c42b1948dbe240d4
sech1p <sech1p@disroot.org>
2021-04-28 12:53:41 +0200

    [youtube] Update Invidious instances urls

--- haruhi_dl/extractor/youtube.py
+++ haruhi_dl/extractor/youtube.py
@@ -285,42 +285,41 @@ class YoutubeIE(YoutubeBaseInfoExtractor):
                             (?:www\.)?hooktube\.com/|
                             (?:www\.)?yourepeat\.com/|
                             tube\.majestyc\.net/|
-                            # Invidious instances taken from https://github.com/omarroth/invidious/wiki/Invidious-Instances
-                            (?:(?:www|dev)\.)?invidio\.us/|
-                            (?:(?:www|no)\.)?invidiou\.sh/|
+                            # Invidious instances taken from https://github.com/iv-org/documentation/blob/master/Invidious-Instances.md and https://uptime.invidious.io
                             (?:(?:www|fi)\.)?invidious\.snopyta\.org/|
-                            (?:www\.)?invidious\.kabi\.tk/|
-                            (?:www\.)?invidious\.13ad\.de/|
-                            (?:www\.)?invidious\.mastodon\.host/|
-                            (?:www\.)?invidious\.zapashcanon\.fr/|
-                            (?:www\.)?invidious\.kavin\.rocks/|
+                            (?:www\.)?yewtu\.be/|
                             (?:www\.)?invidious\.tube/|
-                            (?:www\.)?invidiou\.site/|
+                            (?:www\.)?invidious\.kavin\.rocks/|
+                            (?:www\.)?invidious\.048596\.xyz/|
+                            (?:www\.)?vid\.puffyan\.us/|
+                            (?:www\.)?ytprivate\.com/|
+                            (?:www\.)?invidious\.namazso\.eu/|
+                            (?:www\.)?inv\.skyn3t\.in/|
+                            (?:www\.)?invidious\.himiko\.cloud/|
+                            (?:www\.)?tube\.incog\.host/|
+                            (?:www\.)?invidious\.reallyancient\.tech/|
+                            (?:www\.)?invidious\.silkky\.cloud/|
+                            (?:www\.)?invidious\.exonip\.de/|
+                            (?:www\.)?invidious\.zapashcanon\.fr/|
+                            (?:www\.)?notyoutube\.org/|
+                            # Another Invidious instances
                             (?:www\.)?invidious\.site/|
                             (?:www\.)?invidious\.xyz/|
-                            (?:www\.)?invidious\.nixnet\.xyz/|
-                            (?:www\.)?invidious\.drycat\.fr/|
-                            (?:www\.)?tube\.poal\.co/|
                             (?:www\.)?tube\.connect\.cafe/|
-                            (?:www\.)?vid\.wxzm\.sx/|
                             (?:www\.)?vid\.mint\.lgbt/|
-                            (?:www\.)?yewtu\.be/|
-                            (?:www\.)?yt\.elukerio\.org/|
                             (?:www\.)?yt\.lelux\.fi/|
-                            (?:www\.)?invidious\.ggc-project\.de/|
-                            (?:www\.)?yt\.maisputain\.ovh/|
-                            (?:www\.)?invidious\.13ad\.de/|
-                            (?:www\.)?invidious\.toot\.koeln/|
                             (?:www\.)?invidious\.fdn\.fr/|
-                            (?:www\.)?watch\.nettohikari\.com/|
+                            # TOR Invidious instances
                             (?:www\.)?kgg2m7yk5aybusll\.onion/|
                             (?:www\.)?qklhadlycap4cnod\.onion/|
-                            (?:www\.)?axqzx4s6s54s32yentfqojs3x5i7faxza6xo3ehd4bzzsg2ii4fv2iid\.onion/|
                             (?:www\.)?c7hqkpkpemu6e7emz5b4vyz7idjgdvgaaa3dyimmeojqbgpea3xqjoid\.onion/|
-                            (?:www\.)?fz253lmuao3strwbfbmx46yu7acac2jz27iwtorgmbqlkurlclmancad\.onion/|
-                            (?:www\.)?invidious\.l4qlywnpwqsluw65ts7md3khrivpirse744un3x7mlskqauz5pyuzgqd\.onion/|
+                            (?:www\.)?w6ijuptxiku4xpnnaetxvnkc5vqcdu7mgns2u77qefoixi63vbvnpnqd\.onion/|
+                            (?:www\.)?kbjggqkzv65ivcqj6bumvp337z6264huv5kpkwuv6gu5yjiskvan7fad\.onion/|
+                            (?:www\.)?grwp24hodrefzvjjuccrkw3mjq4tzhaaq32amf33dzpmuxe7ilepcmad\.onion/|
+                            (?:www\.)?invidious\.himiko7xl2skojc6odi7hykl626gt4qki3vxdbv33u2u3af76d6k32ad\.onion/|
+                            (?:www\.)?tuberyps2pn6dor6h47brof3w2asmauahhk4ei42krugybzzzo55klad\.onion/|
+                            (?:www\.)?vipzc556nzjraiqsk5xwvyhz54fvtqt3vrepdarat3zsj4a6eypq\.b32\.i2p/|
                             (?:www\.)?owxfohz4kjyv25fvlqilyxast7inivgiktls3th44jhk3ej3i7ya\.b32\.i2p/|
-                            (?:www\.)?4l2dgddgsrkf2ous66i6seeyi6etzfgrue332grh2n7madpwopotugyd\.onion/|
                             youtube\.googleapis\.com/)                        # the various hostnames, with wildcard subdomains
                          (?:.*?\#/)?                                          # handle anchor (#/) redirect urls
                          (?:                                                  # the various things that can precede the ID:
